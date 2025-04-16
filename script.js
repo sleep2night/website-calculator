21da9435a94710c31d4d2ced47a0012ab1536b72
Roy <roypark2005@gmail.com>
2025-04-15 22:44:01 -0700

    Created functions for adding, subtracting, multiplying, dividing

---
+++ script.js
@@ -0,0 +1,19 @@
+// adds two numbers
+function add(num1, num2){
+    return num1 + num2;
+}
+
+// subtracts two numbers
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+// multiplies two numbers
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+// divides two numbers
+function divide(num1, num2){
+    return num1 / num2;
+}
